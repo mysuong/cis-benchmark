fd5d7e5829afd967a4eda14874fe0b5430678855
MSI\hongd <hongduong7742@gmail.com>
2023-07-13 23:04:16 +0700

    update

--- test/3.5.2.11.sh
+++ test/3.5.2.11.sh
@@ -3,6 +3,6 @@
 
 # 3.5.2.1.11 - Ensure nftables rules are permanent (Automated) - Server1 Workstation1
 
-out=$( awk '/hook input/,/}/' $(awk '$1 ~ /^\s*include/ { gsub("\"","",$2);print $2 }' /etc/sysconfig/nftables.conf)) || exit $?
+out=$(awk '/hook input/,/}/' $(awk '$1 ~ /^\s*include/ { gsub("\"","",$2);print $2 }' /etc/sysconfig/nftables.conf)) || exit $?
 out=$(awk '/hook forward/,/}/' $(awk '$1 ~ /^\s*include/ { gsub("\"","",$2);print $2 }' /etc/sysconfig/nftables.conf)) || exit $?
 
